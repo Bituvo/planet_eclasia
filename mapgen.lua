86a2f2d0b18f28cdb784bd8b6a331ccb06abd6ad
Bituvo <bituvocoding@gmail.com>
2023-12-02 09:43:32 -0500

    Indentation

--- mapgen.lua
+++ mapgen.lua
@@ -1,14 +1,14 @@
 local main_height_params = {
-	offset = planet_eclasia.start_y + 60,
-	scale = 10,
-	spread = {x=280, y=280, z=280},
+    offset = planet_eclasia.start_y + 60,
+    scale = 10,
+    spread = {x=280, y=280, z=280},
     seed = 1
 }
 
 local hill_height_params = {
-	offset = planet_eclasia.start_y + 60,
-	scale = 15,
-	spread = {x=150, y=150, z=150},
+    offset = planet_eclasia.start_y + 60,
+    scale = 15,
+    spread = {x=150, y=150, z=150},
     octaves = 4,
     persistence = 0.7,
     flags = "eased",
@@ -20,7 +20,7 @@ local C_WATER = minetest.get_content_id("default:water_source")
 local C_BEDROCK = C_STONE
 
 if minetest.get_modpath("bedrock") then
-	C_BEDROCK = minetest.get_content_id("bedrock:bedrock")
+    C_BEDROCK = minetest.get_content_id("bedrock:bedrock")
 end
 
 local main_height_perlin_map = {}
@@ -32,15 +32,15 @@ minetest.register_on_generated(function(minp, maxp)
     end
 
     local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
-	local data = vm:get_data()
+    local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
+    local data = vm:get_data()
 
     local side_length = maxp.x - minp.x + 1
-	local map_lengths_xyz = {x=side_length, y=side_length, z=side_length}
+    local map_lengths_xyz = {x=side_length, y=side_length, z=side_length}
 
     main_height_perlin = minetest.get_perlin_map(main_height_params, map_lengths_xyz)
     hill_height_perlin = minetest.get_perlin_map(hill_height_params, map_lengths_xyz)
-	main_height_perlin:get_2d_map_flat({x=minp.x, y=minp.z}, main_height_perlin_map)
+    main_height_perlin:get_2d_map_flat({x=minp.x, y=minp.z}, main_height_perlin_map)
     hill_height_perlin:get_2d_map_flat({x=minp.x, y=minp.z}, hill_height_perlin_map)
 
     local index = 1
@@ -55,10 +55,10 @@ minetest.register_on_generated(function(minp, maxp)
 
                     if y == planet_eclasia.start_y then
                         data[index] = C_BEDROCK
-                    
+
                     elseif y <= terrain_y then
                         data[index] = C_STONE
-                    
+
                     elseif y <= planet_eclasia.start_y + 55 then
                         data[index] = C_WATER
                     end
